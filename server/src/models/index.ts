7fc8e3f770802e30eaeb34059e87b9604f79651c
Sammy Kordi <sammy.kordi@gmail.com>
2024-10-01 14:24:01 -0700

    Fix for localhost

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -6,14 +6,20 @@ import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'postgres',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+  ? new Sequelize(process.env.DB_URL)  // If using a full DB URL, use that
+  : new Sequelize(
+      process.env.DB_NAME || '', 
+      process.env.DB_USER || '', 
+      process.env.DB_PASSWORD || '', 
+      {
+        host: process.env.DB_HOST || 'localhost',   // Use the DB_HOST from environment
+        port: parseInt(process.env.DB_PORT || '5432', 10),  // Use the DB_PORT from environment
+        dialect: 'postgres', 
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+    );
 
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
